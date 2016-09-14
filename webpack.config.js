a9eed3c5d83b885cc4a4297292d7a34cfad81780
sunyunchao <sunyunchao@meituan.com>
2016-09-14 16:35:59 +0800

    测试

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-woshi`var webpack = require('webpack');
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
