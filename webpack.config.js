deba571983378a1fb9123cb99d5589291ac545aa
ranzhu123 <986364200@qq.com>
2016-09-14 17:53:41 +0800

    简洁

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-as var webpack = require('webpack');
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
